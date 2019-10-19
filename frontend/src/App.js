ddd502c3e1e95e28ecf29397782ebacf93f2266f
Takuya <takuya.miraidenshi@gmail.com>
2019-10-19 23:55:57 +0900

    feat: add user feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
-import Board from './components/pages/Board';
+import Board from './components/pages/Board/ClanIndex.js';
+import UserList from './components/pages/Board/UserIndex.js';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <Board />
+      <UserList />
     </div>
   );
 }
