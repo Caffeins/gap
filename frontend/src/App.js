7e488597ded8ff784ee60037a04d049b29f94081
Takuya <takuya.miraidenshi@gmail.com>
2019-10-20 01:23:44 +0900

    feat: set router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,6 @@ function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Board />
-      <UserList />
     </div>
   );
 }
--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -6,6 +6,9 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import axios from 'axios';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import UserList from '../../components/pages/Board/UserIndex.js';
+import Board from '../../components/pages/Board/ClanIndex.js';
 
 class Sidebar extends Component {
   constructor() {
@@ -25,52 +28,70 @@ class Sidebar extends Component {
 
   render() {
     return (
-      <div>
-        <Drawer variant="permanent" anchor="left">
-          <List>
-            <ListItem>
-              <Avatar src={this.state.user.image} />
-              <ListItemText primary={this.state.user.name} />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary={`場所：${this.state.user.place}`} />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary={`趣味：${this.state.user.hobby}`} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem button>
-              <ListItemText primary={'プロフィールを編集'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'所属クラン'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'参加しているイベント'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'フォロワー/フォロワー'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'クランを作成'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'イベントを作成'} />
-            </ListItem>
-            <ListItem button>
-              <ListItemText primary={'フォローする/はずす'} />
-            </ListItem>
-          </List>
-          <Divider />
-          <List>
-            <ListItem button>
-              <ListItemText primary={'ログアウト'} />
-            </ListItem>
-          </List>
-        </Drawer>
-      </div>
+      <Router>
+        <div>
+          <Drawer variant="permanent" anchor="left">
+            <List>
+              <ListItem>
+                <Avatar src={this.state.user.image} />
+                <ListItemText primary={this.state.user.name} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={`場所：${this.state.user.place}`} />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary={`趣味：${this.state.user.hobby}`} />
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <Link to="/">
+                <ListItem button>
+                  <ListItemText primary={'ホーム'} />
+                </ListItem>
+              </Link>
+              <ListItem button>
+                <ListItemText primary={'プロフィールを編集'} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary={'所属クラン'} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary={'参加しているイベント'} />
+              </ListItem>
+              <Link to="/follow">
+                <ListItem button>
+                  <ListItemText primary={'フォロー/フォロワー'} />
+                </ListItem>
+              </Link>
+              <ListItem button>
+                <ListItemText primary={'クランを作成'} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary={'イベントを作成'} />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary={'フォローする/はずす'} />
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem button>
+                <ListItemText primary={'ログアウト'} />
+              </ListItem>
+            </List>
+          </Drawer>
+
+          <Switch>
+            <Route path="/follow">
+              <UserList />
+            </Route>
+            <Route path="/">
+              <Board />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
