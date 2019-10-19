cb0e019193165ea4ef0ea4f1ca9bbe1d5d6474d4
umepon0626 <ryuichi-astrona@ymobile.ne.jp>
2019-10-19 15:56:59 +0900

    create_demo_boards

---
+++ frontend/src/components/pages/Board/Keiji/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+
+class EventBoard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Paper>
+          <Box>タイトル</Box>
+          <Box>説明</Box>
+          {/* <User_list/> */}
+          <div>user_list</div>
+          <Button variant="contained" color="primary">
+            詳細
+          </Button>
+          <Button variant="contained" color="secondary">
+            参加する
+          </Button>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default EventBoard;
