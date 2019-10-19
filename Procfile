8b8c526323c7880f851be439beb4173a7ddb9350
Mateheath <mateheath12195@gmail.com>
2019-10-19 20:20:07 +0900

    This commit for heroku deploy

---
+++ Procfile
@@ -0,0 +1,4 @@
+
+
+release: python manage.py migrate
+web: gunicorn caffein-gap.wsgi --log-file -
--- gap/gap/settings.py
+++ gap/gap/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import django_heroku 
+django_heroku.settings(locals())
 import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
